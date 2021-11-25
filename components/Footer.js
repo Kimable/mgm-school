8c6b2637663eb481e752f891ba898077cd201f48
Kimable <kkimani@yahoo.com>
2021-11-25 18:59:12 +0300

    removed a charater from footer to streamline deployment

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <div className="container">
-        <p>Copyright &copy; 2021 | MGM Boys' Academy</p>
+        <p>Copyright &copy; 2021 | MGM Boys Academy</p>
       </div>
     </footer>
   );
